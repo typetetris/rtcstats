b78a094b67b51b6e301639e272503f1899376904
Eric Wolf <ericwolf42@gmail.com>
2022-06-27 17:03:23 +0200

    Remove collection of deprecated special google statistics

--- rtcstats.js
+++ rtcstats.js
@@ -113,7 +113,6 @@ module.exports = function(trace, getStatsInterval, prefixesToWrap) {
   var peerconnectioncounter = 0;
   var isFirefox = !!window.mozRTCPeerConnection;
   var isEdge = !!window.RTCIceGatherer;
-  var isSafari = !isFirefox && window.RTCPeerConnection && !window.navigator.webkitGetUserMedia;
   prefixesToWrap.forEach(function(prefix) {
     if (!window[prefix + 'RTCPeerConnection']) {
       return;
@@ -185,23 +184,16 @@ module.exports = function(trace, getStatsInterval, prefixesToWrap) {
       });
 
       var prev = {};
-      var googlePrev = {};
       var getStats = function() {
         pc.getStats(null).then(function(res) {
           var now = map2obj(res);
+          // trace('getstatsRaw', id, JSON.parse(JSON.stringify(now)));
           var base = JSON.parse(JSON.stringify(now)); // our new prev
           trace('getstats', id, deltaCompression(prev, now));
           prev = base;
         });
-        if (!isFirefox && !isSafari) {
-          pc.getStats(function(res) {
-            var now = mangleChromeStats(pc, res);
-            var base = JSON.parse(JSON.stringify(now)); // our new prev
-            trace('getstatsGoogle', id, deltaCompression(googlePrev, now));
-            googlePrev = base;
-          });
-        }
       };
+
       // TODO: do we want one big interval and all peerconnections
       //    queried in that or one setInterval per PC?
       //    we have to collect results anyway so...
